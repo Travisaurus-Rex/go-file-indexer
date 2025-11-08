b6d8b89d93af95edf0d82efd67cb312bd0c17731
Travis Adams <brokenCD@live.com>
2025-11-08 08:51:01 +0700

    add mimetype to file info

--- internal/scanner/walker.go
+++ internal/scanner/walker.go
@@ -1,6 +1,7 @@
 package scanner
 
 import (
+	"mime"
 	"os"
 	"path/filepath"
 	"time"
@@ -9,6 +10,7 @@ import (
 type FileInfo struct {
 	Path      string    `json:"path"`
 	SizeBytes int64     `json:"size_bytes"`
+	MimeType  string    `json:"mime_type"`
 	Modified  time.Time `json:"modified"`
 }
 
@@ -29,9 +31,17 @@ func ScanDir(root string) ([]FileInfo, error) {
 			return nil
 		}
 
+		ext := filepath.Ext(path)
+		mimeType := mime.TypeByExtension(ext)
+
+		if mimeType == "" {
+			mimeType = "application/octet-stream"
+		}
+
 		files = append(files, FileInfo{
 			Path:      path,
 			SizeBytes: info.Size(),
+			MimeType:  mimeType,
 			Modified:  info.ModTime(),
 		})
 
