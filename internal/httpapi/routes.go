78212af6790923bf3c4e5a3b9122ba1f7a145324
Travis Adams <brokenCD@live.com>
2025-11-08 08:42:46 +0700

    replace static files list with file scanner

--- internal/httpapi/routes.go
+++ internal/httpapi/routes.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/Travisaurus-Rex/go-file-indexer/internal/config"
+	"github.com/Travisaurus-Rex/go-file-indexer/internal/scanner"
 )
 
 type FileInfo struct {
@@ -44,25 +45,12 @@ func handleConfig(w http.ResponseWriter, r *http.Request, cfg config.Config) {
 func handleFiles(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	files := []FileInfo{
-		{
-			Path:      "./data/example1.txt",
-			SizeBytes: 12345,
-			MimeType:  "text/plain",
-			Modified:  "2025-11-07T13:00:00z",
-		},
-		{
-			Path:      "./data/image.jpg",
-			SizeBytes: 987654,
-			MimeType:  "image/jpeg",
-			Modified:  "2025-11-07T12:45:00Z",
-		},
-		{
-			Path:      "./data/archive.zip",
-			SizeBytes: 5000000,
-			MimeType:  "application/zip",
-			Modified:  "2025-11-06T09:30:00Z",
-		},
+	root := "./data"
+	files, err := scanner.ScanDir(root)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error":"failed to scan directory"}`))
+		return
 	}
 
 	data, err := json.Marshal(files)
