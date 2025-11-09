5c14b92f0e84c0fc75d0294ce4d3ed29d862d885
Travis Adams <brokenCD@live.com>
2025-11-09 10:45:10 +0700

    update scan directory logic for concurrency

--- internal/scanner/walker.go
+++ internal/scanner/walker.go
@@ -4,6 +4,7 @@ import (
 	"mime"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
@@ -15,38 +16,67 @@ type FileInfo struct {
 }
 
 func ScanDir(root string) ([]FileInfo, error) {
+	const numWorkers = 4
+	var paths = make(chan string, 100)
 	var files []FileInfo
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var walkErr error
 
-	err := filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
-		if err != nil {
+	go func() {
+		defer close(paths)
+		walkErr = filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
+			if err != nil {
+				return nil
+			}
+
+			if d.IsDir() {
+				return nil
+			}
+
+			paths <- path
 			return nil
-		}
-
-		if d.IsDir() {
-			return nil
-		}
-
-		info, err := d.Info()
-		if err != nil {
-			return nil
-		}
-
-		ext := filepath.Ext(path)
-		mimeType := mime.TypeByExtension(ext)
-
-		if mimeType == "" {
-			mimeType = "application/octet-stream"
-		}
-
-		files = append(files, FileInfo{
-			Path:      path,
-			SizeBytes: info.Size(),
-			MimeType:  mimeType,
-			Modified:  info.ModTime(),
 		})
 
-		return nil
-	})
+	}()
 
-	return files, err
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			for path := range paths {
+
+				info, err := os.Stat(path)
+				if err != nil {
+					continue
+				}
+
+				ext := filepath.Ext(path)
+				mimeType := mime.TypeByExtension(ext)
+
+				if mimeType == "" {
+					mimeType = "application/octet-stream"
+				}
+
+				file := FileInfo{
+					Path:      path,
+					SizeBytes: info.Size(),
+					MimeType:  mimeType,
+					Modified:  info.ModTime(),
+				}
+				mu.Lock()
+				files = append(files, file)
+				mu.Unlock()
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	if walkErr != nil {
+		return nil, walkErr
+	}
+
+	return files, nil
 }
