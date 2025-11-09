ca8004aa1978069ce1405dfe6e5206d1c1cfc904
Travis Adams <brokenCD@live.com>
2025-11-09 11:10:41 +0700

    split up go routines to named functions for readability

--- internal/scanner/walker.go
+++ internal/scanner/walker.go
@@ -1,6 +1,7 @@
 package scanner
 
 import (
+	"fmt"
 	"mime"
 	"os"
 	"path/filepath"
@@ -25,51 +26,12 @@ func ScanDir(root string) ([]FileInfo, error) {
 
 	go func() {
 		defer close(paths)
-		walkErr = filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
-			if err != nil {
-				return nil
-			}
-
-			if d.IsDir() {
-				return nil
-			}
-
-			paths <- path
-			return nil
-		})
-
+		walkErr = walkDirectory(root, paths)
 	}()
 
-	for i := 0; i < numWorkers; i++ {
+	for i := range numWorkers {
 		wg.Add(1)
-
-		go func() {
-			defer wg.Done()
-			for path := range paths {
-
-				info, err := os.Stat(path)
-				if err != nil {
-					continue
-				}
-
-				ext := filepath.Ext(path)
-				mimeType := mime.TypeByExtension(ext)
-
-				if mimeType == "" {
-					mimeType = "application/octet-stream"
-				}
-
-				file := FileInfo{
-					Path:      path,
-					SizeBytes: info.Size(),
-					MimeType:  mimeType,
-					Modified:  info.ModTime(),
-				}
-				mu.Lock()
-				files = append(files, file)
-				mu.Unlock()
-			}
-		}()
+		go worker(i, paths, &files, &mu, &wg)
 	}
 
 	wg.Wait()
@@ -79,4 +41,50 @@ func ScanDir(root string) ([]FileInfo, error) {
 	}
 
 	return files, nil
+}
+
+func walkDirectory(root string, paths chan<- string) error {
+	return filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		paths <- path
+		return nil
+	})
+}
+
+func worker(id int, paths <-chan string, files *[]FileInfo, mu *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for path := range paths {
+
+		info, err := os.Stat(path)
+		if err != nil {
+			continue
+		}
+
+		ext := filepath.Ext(path)
+		mimeType := mime.TypeByExtension(ext)
+
+		if mimeType == "" {
+			mimeType = "application/octet-stream"
+		}
+
+		file := FileInfo{
+			Path:      path,
+			SizeBytes: info.Size(),
+			MimeType:  mimeType,
+			Modified:  info.ModTime(),
+		}
+		mu.Lock()
+		*files = append(*files, file)
+		mu.Unlock()
+
+		fmt.Printf("[worker %d] processed %s\n", id, filepath.Base(path))
+	}
+
 }
